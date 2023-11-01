24c7e4605f58f26fa0e9cbe26ec024c108676c07
PedroYep <psyepez2@utpl.edu.ec>
2023-11-01 16:24:39 -0500

    AreaTriangulo

---
+++ APE_Senama4_Soluciones_PedroYep/src/ape_senama4_soluciones_pedroyep/APE_Senama4_Soluciones_PedroYep.java
@@ -0,0 +1,12 @@
+
+package ape_senama4_soluciones_pedroyep;
+public class APE_Senama4_Soluciones_PedroYep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ APE_Senama4_Soluciones_PedroYep/src/ape_senama4_soluciones_pedroyep/AreaTriangulo_1.java
@@ -0,0 +1,21 @@
+/*
+Algoritmo-1
+Generar un algoritmo que permite calcular y presentar el área de un triángulo. 
+Los datos deben ser pedidos al usuario.
+*/
+package ape_senama4_soluciones_pedroyep;
+import java.util.Scanner;          
+public class AreaTriangulo_1 {
+    public static void main(String[] args) {
+        double baseTriangulo;
+        double alturaTriangulo;
+        double areaTriangulo;
+        Scanner teclado = new Scanner(System.in); //INICIALIZAR E INICIALIZAR
+        System.out.println("Ingrese la base del triangulo: ");
+        baseTriangulo = teclado.nextDouble();
+        System.out.println("Ingrese la altura del triangulo: ");
+        alturaTriangulo = teclado.nextDouble();
+        areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+        System.out.println("El area del triangulo es: " + areaTriangulo);
+    }
+}
